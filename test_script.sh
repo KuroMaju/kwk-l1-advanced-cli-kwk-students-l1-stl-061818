3631148bd50bc764716a5e6632cb8cf9ae6e0c20
KuroMaju <reimicarter@gmail.com>
2018-06-18 19:09:52 +0000

    automatically backed up by learn

--- test_script.sh
+++ test_script.sh
@@ -7,4 +7,4 @@ this_is_a_directory_made_in_a_script/first_file.txt;
 touch
 this_is_a_directory_made_in_a_script/second_file;
 mv
-this_is_a_directory_made_in_a_script/second_file.txt another_directory
+this_is_a_directory_made_in_a_script/second_file.txt another_directory;
